535eda446a2075365a678ca650c11b997d3af6be
Alex Di Domenico <alexxdude@gmail.com>
2015-04-23 21:12:20 -0400

    Created login/out system

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
 
+  get 'sessions/new'
+
   root   'welcome#home'
   get 'about' => 'welcome#about'
+
   get 'signup' => 'users#new'
+  
+  get 'login' => 'sessions#new'
+  post 'login' => 'sessions#create'
+  delete 'logout' => 'sessions#destroy'
+
   resources :users
   
 end
